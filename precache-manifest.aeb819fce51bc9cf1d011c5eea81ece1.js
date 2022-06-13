08e57014234f898590815607208577e7870efee7
Dmitry Milya <dmilya2000@gmail.com>
2022-06-13 09:40:00 +0300

    Updates

---
+++ precache-manifest.aeb819fce51bc9cf1d011c5eea81ece1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5d82ab02cbb9893f65ecdaf76e8098",
+    "url": "/dalle-playground/index.html"
+  },
+  {
+    "revision": "94d58177dde6b6b38e4c",
+    "url": "/dalle-playground/static/css/main.b14d694a.chunk.css"
+  },
+  {
+    "revision": "910650e38c7e435cba2c",
+    "url": "/dalle-playground/static/js/2.b02db43c.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/dalle-playground/static/js/2.b02db43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d58177dde6b6b38e4c",
+    "url": "/dalle-playground/static/js/main.e5cdf028.chunk.js"
+  },
+  {
+    "revision": "2287b2335ed2078787ec",
+    "url": "/dalle-playground/static/js/runtime-main.f1925457.js"
+  }
+]);
